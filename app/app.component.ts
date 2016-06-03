7763fcd1d1d18b1fd38622f59dceeae816fc9f08
Mario Ojeda <marioojeda@MacBook-Pro-de-Mario.local>
2016-06-03 08:51:14 -0300

    Course component and *ngFor = #course in courses

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core';
-
+import {CoursesComponent} from './courses.component'
 @Component({
     selector: 'my-app',
-    template: '<h1>Hola Angular 2</h1>'
+    template: '<h1>Hola Angular 2</h1><courses></courses>',
+    directives: [CoursesComponent]
 })
 export class AppComponent { }
